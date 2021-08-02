48f59c32eec0b0e8892fe401fad1613432f5abdf
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-08-01 22:55:48 -0700

    server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,22 @@
 // build your server here and require it from index.js
+const express = require('express');
+const tasksRouter = require('./tasks/tasks-router');
+const resourceRouter = require('./resource/resource-router');
+const projectRouter = require('./project/project-router');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/recipes', tasksRouter);
+server.use('/api/resource', resourceRouter);
+server.use('/api/project', projectRouter);
+
+server.use('*',
+  (req, res) => {
+    res.json({
+      api: 'alive'
+    });
+  });
+
+module.exports = server;
