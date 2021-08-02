12619989df273c0c94665aa9373f8cc80d393015
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-08-01 23:29:25 -0700

    wip task

--- api/task/model.js
+++ api/task/model.js
@@ -1,5 +1,33 @@
 // build your `Task` model here
 const db = require('../../data/db-config');
 
+module.exports = {
+  getTasks,
+  addTask,
+  getTasksByProjectId
+};
 
-module.exports = {  };
+function getTasks() {
+  return db('tasks')
+    .join('projects', 'tasks.projectId', 'projects.id')
+    .select(
+      'tasks.id',
+      { taskDesc: 'tasks.description' },
+      'tasks.notes',
+      'tasks.completed',
+      { projName: 'projects.name' },
+      { projDesc: 'projects.description' }
+    );
+}
+
+function addTask(newTask) {
+  return db('tasks')
+    .insert(newTask)
+    .then((id) => {
+      return newTask;
+    });
+}
+
+function getTasksByProjectId(id) {
+  return db('tasks').where({ 'tasks.projectId': id });
+}
--- api/task/router.js
+++ api/task/router.js
@@ -2,22 +2,43 @@
 const router = require('express').Router();
 const Task = require('./model');
  
-router.get('/:project_id',
-  (req,res,next) => {
+router.get('/', (req, res) => {
+  Task.getTasks()
+    .then((tasks) => {
+      res.json(tasks);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'Failed to get tasks' });
+    });
+});
 
-  });
+router.get('/:id/tasks', (req, res) => {
+  const { id } = req.params;
+
+  Task.getTasksByProjectId(id)
+    .then((tasks) => {
+      if (tasks.length) {
+        res.json(tasks);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'Could not find tasks for given project' });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'Failed to get tasks' });
+    });
+});
 
 
+router.post('/tasks', (req, res) => {
+  const newTask = req.body;
 
-//// the pit ///////
-
-  router.use( 
-    (err,req,res,next) => { //eslint-disable-line
-      res.json({
-        customMessage: "\nIt's dangerous to go alone;\n take this chance to visit stackOverflow",
-        message: err.message,
-        stack: err.stack
-      });
-  });
-
-module.exports = router;// build your `/api/tasks` router here
+  Task.addTask(newTask)
+    .then((task) => {
+      res.status(201).json(task);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'Failed to create new task' });
+    });
+});
